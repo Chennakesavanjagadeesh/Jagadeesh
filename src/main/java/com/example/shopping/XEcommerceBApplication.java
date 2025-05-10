89b7fef6a81f79fb68a9ea4ae9cfaf923c2b31ec
Chennakesavanjagadeesh <141047108+Chennakesavanjagadeesh@users.noreply.github.com>
2025-05-11 00:44:21 +0530

    jaga

---
+++ src/main/java/com/example/shopping/XEcommerceBApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XEcommerceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XEcommerceBApplication.class, args);
+	}
+
+}
